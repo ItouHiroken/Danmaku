872242a31c1d34a0397cc1028191c5f82f7098d6
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-11-21 19:46:42 +0900

    [Fix]変数名がおかしかったので変更した

--- Assets/Danmaku.cs
+++ Assets/Danmaku.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Danmaku : MonoBehaviour
 {
     [SerializeField] GameObject _bullet;
-    [SerializeField] float _time = 1f;
     /// <summary>360Ç≈äÑÇÍÇÈêîéöì¸ÇÍÇƒÇŸÇµÇ¢</summary>
-    [SerializeField,Range(0,20)] int _howMany;
-    [SerializeField] float _guruguruTime = 0;
-    [SerializeField] float _happaTime = 0;
+    [SerializeField, Range(0, 20)] int _howMany;
+    [SerializeField] float _gameTime = 0;
+    [SerializeField, Range(0, 1)] float _happaFormChangeIntervalTime = 0.1f;
+    [SerializeField, Range(0, 1)] float _guruguruFormChangeIntervalTime = 0.1f;
     [SerializeField] CombatForm _form;
 
     private void Start()
@@ -18,7 +18,7 @@ public class Danmaku : MonoBehaviour
     }
     private void Update()
     {
-        _guruguruTime += Time.deltaTime;
+        _gameTime += Time.deltaTime;
     }
     enum CombatForm
     {
@@ -46,11 +46,11 @@ public class Danmaku : MonoBehaviour
             Debug.Log(Angle);
             GameObject BulletQueen = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
             Rigidbody2D BulletChanRB = BulletQueen.GetComponent<Rigidbody2D>();
-            BulletQueen.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime) * 360);
-            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _guruguruTime) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _guruguruTime);
+            BulletQueen.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _gameTime) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _gameTime) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _gameTime);
             Destroy(BulletQueen, 10f);
         }
-        yield return new WaitForSeconds(_time);
+        yield return new WaitForSeconds(_guruguruFormChangeIntervalTime);
         SummonPattern();
     }
     IEnumerator HappaSummonBullet()
@@ -62,8 +62,8 @@ public class Danmaku : MonoBehaviour
             Debug.Log(Angle);
             GameObject BulletChan = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
             Rigidbody2D BulletChanRB = BulletChan.GetComponent<Rigidbody2D>();
-            BulletChan.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i) * 360);
-            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _guruguruTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i);
+            BulletChan.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _gameTime + Mathf.PI / 2 * i) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _gameTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _gameTime + Mathf.PI / 2 * i);
             Destroy(BulletChan, 10f);
         }
         //Ç±ÇÍÇÕîΩéûåvâÒÇË
@@ -73,11 +73,11 @@ public class Danmaku : MonoBehaviour
             Debug.Log(Angle);
             GameObject BulletKing = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
             Rigidbody2D BulletChanRB = BulletKing.GetComponent<Rigidbody2D>();
-            BulletKing.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i) * 360);
-            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle - _guruguruTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle - _guruguruTime + Mathf.PI / 2 * i);
+            BulletKing.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _gameTime + Mathf.PI / 2 * i) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle - _gameTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle - _gameTime + Mathf.PI / 2 * i);
             Destroy(BulletKing, 10f);
         }
-        yield return new WaitForSeconds(_happaTime);
+        yield return new WaitForSeconds(_happaFormChangeIntervalTime);
         SummonPattern();
     }
 }
