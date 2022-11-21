4276814e7967f390eb31259395eab37ab8cf0a77
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-11-21 19:40:07 +0900

    [Update]弾幕を二種類作った、切り替えできるようにした

--- Assets/Danmaku.cs
+++ Assets/Danmaku.cs
@@ -7,31 +7,77 @@ public class Danmaku : MonoBehaviour
     [SerializeField] GameObject _bullet;
     [SerializeField] float _time = 1f;
     /// <summary>360で割れる数字入れてほしい</summary>
-    [SerializeField] int _howMany;
+    [SerializeField,Range(0,20)] int _howMany;
     [SerializeField] float _guruguruTime = 0;
+    [SerializeField] float _happaTime = 0;
+    [SerializeField] CombatForm _form;
 
     private void Start()
     {
-        StartCoroutine(SummonBullet());
+        StartCoroutine(GuruguruSummonBullet());
     }
     private void Update()
     {
         _guruguruTime += Time.deltaTime;
     }
-    IEnumerator SummonBullet()
+    enum CombatForm
     {
-        Debug.Log("しょうかん！");
+        Guruguru,
+        Happa,
+    }
+    void SummonPattern()
+    {
+        switch (_form)
+        {
+            case CombatForm.Guruguru:
+                StartCoroutine(GuruguruSummonBullet());
+                break;
+            case CombatForm.Happa:
+                StartCoroutine(HappaSummonBullet());
+                break;
+        }
+    }
+    IEnumerator GuruguruSummonBullet()
+    {
+        Debug.Log("ぐるぐるしょうかん！");
+        for (int i = 0; (2 * Mathf.PI / _howMany) * i < 2 * Mathf.PI; i++)
+        {
+            float Angle = (2 * Mathf.PI / _howMany) * i;
+            Debug.Log(Angle);
+            GameObject BulletQueen = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
+            Rigidbody2D BulletChanRB = BulletQueen.GetComponent<Rigidbody2D>();
+            BulletQueen.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _guruguruTime) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _guruguruTime);
+            Destroy(BulletQueen, 10f);
+        }
+        yield return new WaitForSeconds(_time);
+        SummonPattern();
+    }
+    IEnumerator HappaSummonBullet()
+    {
+        ////これが時計回り
         for (int i = 0; (2 * Mathf.PI / _howMany) * i < 2 * Mathf.PI; i++)
         {
             float Angle = (2 * Mathf.PI / _howMany) * i;
             Debug.Log(Angle);
             GameObject BulletChan = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
             Rigidbody2D BulletChanRB = BulletChan.GetComponent<Rigidbody2D>();
-            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _guruguruTime) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _guruguruTime);
+            BulletChan.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle + _guruguruTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i);
             Destroy(BulletChan, 10f);
         }
-
-        yield return new WaitForSeconds(_time);
-        StartCoroutine(SummonBullet());
+        //これは反時計回り
+        for (int i = 0; (2 * Mathf.PI / _howMany) * i < 2 * Mathf.PI; i++)
+        {
+            float Angle = (2 * Mathf.PI / _howMany) * i;
+            Debug.Log(Angle);
+            GameObject BulletKing = Instantiate(_bullet, gameObject.transform.position, gameObject.transform.rotation);
+            Rigidbody2D BulletChanRB = BulletKing.GetComponent<Rigidbody2D>();
+            BulletKing.transform.Rotate(new Vector3(0, 0, 1) * Mathf.Cos(Angle + _guruguruTime + Mathf.PI / 2 * i) * 360);
+            BulletChanRB.velocity = new Vector3(1, 0, 0) * Mathf.Sin(Angle - _guruguruTime + Mathf.PI / 2 * i) + new Vector3(0, 1, 0) * Mathf.Cos(Angle - _guruguruTime + Mathf.PI / 2 * i);
+            Destroy(BulletKing, 10f);
+        }
+        yield return new WaitForSeconds(_happaTime);
+        SummonPattern();
     }
 }
